eb814399d71a3b4a70c09ba0ed93373949682d84
Ecrocker11 <evan.crocker@greentechrenewables.com>
2025-07-25 15:39:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -263,6 +263,8 @@ if csv_file and pdf_file:
                 if label in fields:
                     if status.startswith("❌"):
                         st.markdown(f"<span style='color:red'><strong>{label}:</strong> `{value}` → {status}</span>", unsafe_allow_html=True)
+                    if status.startswith("⚠️"):
+                        st.markdown(f"<span style='color:yellow'><strong>{label}:</strong> `{value}` → {status}</span>", unsafe_allow_html=True)
                     else:
                         st.markdown(f"<strong>{label}:</strong> `{value}` → {status}", unsafe_allow_html=True)
                     st.caption(explanation)
